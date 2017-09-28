caa211b811c6b7a9daaf022c7ee9416514023abd
SEDoug <doug.macgregor@gmail.com>
2017-09-28 12:58:42 -0500

    First upload to portfolioapp repository

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad architecto autem commodi corporis doloribus eveniet expedita laudantium minus molestiae, nihil obcaecati, quae, rerum sed suscipit unde. Aspernatur harum optio quaerat',
+            'author': 'Doug MacGregor',
+            'create_date': '09-15-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad architecto autem commodi corporis doloribus eveniet expedita laudantium minus molestiae, nihil obcaecati, quae, rerum sed suscipit unde. Aspernatur harum optio quaerat',
+            'author': 'Joseph Stalin',
+            'create_date': '09-15-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad architecto autem commodi corporis doloribus eveniet expedita laudantium minus molestiae, nihil obcaecati, quae, rerum sed suscipit unde. Aspernatur harum optio quaerat',
+            'author': 'Elon Musk',
+            'create_date': '09-15-2017'
+        },
+    ]
+    return articles
